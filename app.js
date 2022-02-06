729cf7134d2680752cc9a5a45ca6ce680ad52a90
Sachin Ghadage <sachinghadage232002@gmail.com>
2022-02-06 13:55:51 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+
+    const query = req.body.cityName
+    const appid = "80775b9ef24f607b6e452ac70e6cb34e"
+    const unit = "metric"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + appid + "&units=" + unit;
+    https.get(url, function (response) {
+        console.log(response.statusCode);
+
+        response.on("data", function (data) {
+            weatherData = JSON.parse(data)
+            const temp = weatherData.main.temp
+            const weatherDescription = weatherData.weather[0].description
+            const icon = weatherData.weather[0].icon
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+
+            res.write("<p>The weather is currently " + weatherDescription + "</p>")
+            res.write("<h1>The temperature in "+query+" is " + temp + " degree celsis </h1>")
+            res.write("<img src=" + imageURL + ">")
+            res.send()
+        })
+
+    })
+
+})
+
+
+app.listen(2000, function () {
+    console.log("server is running on port 2000")
+})
